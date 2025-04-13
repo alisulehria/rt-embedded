46e363bc92a8f759b67011a2e866b73b8d94e9dd
ali <aliaswa@gmail.com>
2025-04-13 12:55:08 -0600

    ex4 complete

--- sk_exer4/main.cpp
+++ sk_exer4/main.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <thread>
 
+static int state = 0;
+/*
 void fib10Work()
 {
     const unsigned N = 47;
@@ -16,10 +18,11 @@ void fib10Work()
             fib1 = fib;
         }
     }
-}
+} */
 
-void fib20Work()
+void switchState()
 {
+    /*/
     const unsigned N = 47;
     unsigned fib0=0, fib1=1, fib=0;
     for (unsigned i=0; i<56000; i++)
@@ -30,22 +33,34 @@ void fib20Work()
             fib0 = fib1;
             fib1 = fib;
         }
+    } */
+
+    if(state == 0){
+        system("sudo pinctrl set 17 dh");
+        state = 1;
+    }
+    if(state == 1){
+        system("sudo pinctrl set 17 dl");
+        state = 0;
     }
 }
 
 int main()
 {
+
     std::cout << "Starting Sequencer Demo with POSIX Timer + SIGALRM\n";
 
     Sequencer seq;
 
     // addService(func, priority, cpuAffinity, periodMs)
-    seq.addService("fib10Service",fib10Work, /*priority=*/99, /*cpuAffinity=*/0, /*periodMs=*/20);
-    seq.addService("fib20Service",fib20Work, /*priority=*/98, /*cpuAffinity=*/1, /*periodMs=*/50);
+    seq.addService("fib10Service",switchState, /*priority=*/99, /*cpuAffinity=*/0, /*periodMs=*/100);
+    //seq.addService("fib20Service",fib20Work, /*priority=*/98, /*cpuAffinity=*/1, /*periodMs=*/50);
+
+    system("sudo pinctrl 17 op dl");
 
     // Master alarm ticks every 10 ms, for example.
     seq.startServices(/*masterIntervalMs=*/10);
-
+ 
 
     // std::this_thread::sleep_for(std::chrono::seconds(10));
     // seq.stopServices();
